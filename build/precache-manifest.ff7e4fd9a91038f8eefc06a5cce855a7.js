78094981c96bb56068c2164c52a95ed4a2d6c4b9
Pedro Stanislau Tomacheski - PTM <pedro.tomacheski@iteris.com.br>
2022-08-19 02:02:46 -0300

    insta build

---
+++ build/precache-manifest.ff7e4fd9a91038f8eefc06a5cce855a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3bf57b615d18b54269361b3a3d6e07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b5061ee92abc6a1577",
+    "url": "/static/css/main.c2ecd48f.chunk.css"
+  },
+  {
+    "revision": "cbe964fde2277da60b5d",
+    "url": "/static/js/2.5942a445.chunk.js"
+  },
+  {
+    "revision": "b7b5061ee92abc6a1577",
+    "url": "/static/js/main.7bce259d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
